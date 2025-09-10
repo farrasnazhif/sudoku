0bb7d3be05713847a290190ccbce267724d4b216
farrasnazhif <farrasnazhif@gmail.com>
2025-09-10 21:01:57 +0700

    grid css

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -2,13 +2,13 @@ import { range } from "../utils";
 
 const Grid = () => {
   return (
-    <div className="container">
-      <table className="table">
+    <div className="grid-container">
+      <table className="grid-table">
         <tbody>
           {range(9).map((row) => (
             <tr key={row}>
               {range(9).map((col) => (
-                <td key={col}>
+                <td key={col} className="col-cell">
                   <input type="text" />
                 </td>
               ))}
