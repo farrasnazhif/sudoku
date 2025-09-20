a383e4c3200e8b2e0ac68a2cd2045e3ba79558ee
farrasnazhif <farrasnazhif@gmail.com>
2025-09-20 08:30:30 +0700

    box css and handle user input

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,47 @@
+import React from "react";
 import "./App.css";
 import Grid from "./components/Grid";
+import { range } from "./utils";
+
+// set all board will null value
+const emptyBoard = () => range(9).map(() => range(9).map(() => null));
 
 function App() {
+  // puzzle for the api
+  const [puzzle, setPuzzle] = React.useState(emptyBoard);
+  // board value from user input
+  const [board, setBoard] = React.useState(emptyBoard);
+
+  // [row, col]
+  const [selected, setSelected] = React.useState(null);
+
+  // handle user input
+  const handleInput = (rIdx, cIdx, value) => {
+    if (value === "" || (value >= 1 && value <= 9)) {
+      setBoard((prev) =>
+        prev.map((row, r) =>
+          row.map((cell, c) => {
+            if (r === rIdx && c === cIdx) {
+              return value ? parseInt(value) : null;
+            }
+
+            return cell;
+          })
+        )
+      );
+    }
+  };
+
   return (
     <div style={{ textAlign: "center" }}>
       <h1>SUDOKU</h1>
-      <Grid />
+      <Grid
+        selected={selected}
+        setSelected={setSelected}
+        board={board}
+        puzzle={puzzle}
+        handleInput={handleInput}
+      />
     </div>
   );
 }
--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,17 +1,45 @@
-import { range } from "../utils";
+import classNames from "classnames";
 
-const Grid = () => {
+const Grid = ({ board, puzzle, selected, setSelected, handleInput }) => {
   return (
     <div className="grid-container">
       <table className="grid-table">
         <tbody>
-          {range(9).map((row) => (
-            <tr key={row}>
-              {range(9).map((col) => (
-                <td key={col} className="col-cell">
-                  <input type="text" />
-                </td>
-              ))}
+          {/* map board by row and each cell in the row */}
+          {board.map((row, rIdx) => (
+            <tr key={rIdx}>
+              {row.map((cell, cIdx) => {
+                const isPrefilled = puzzle[rIdx][cIdx] !== null;
+                const isSelected =
+                  selected && rIdx === selected[0] && cIdx === selected[1];
+
+                return (
+                  <td
+                    key={cIdx}
+                    className={classNames("cell", {
+                      "same-row": selected && rIdx === selected[0],
+                      "same-col": selected && cIdx === selected[1],
+                      "same-box":
+                        selected &&
+                        Math.floor(rIdx / 3) === Math.floor(selected[0] / 3) &&
+                        Math.floor(cIdx / 3) === Math.floor(selected[1] / 3),
+                      selected: isSelected,
+                    })}
+                  >
+                    <input
+                      type="text"
+                      maxLength={1}
+                      value={cell ?? ""}
+                      readOnly={isPrefilled}
+                      onFocus={() => setSelected([rIdx, cIdx])}
+                      onClick={() => setSelected([rIdx, cIdx])}
+                      onChange={(event) => {
+                        handleInput(rIdx, cIdx, event.target.value);
+                      }}
+                    />
+                  </td>
+                );
+              })}
             </tr>
           ))}
         </tbody>
